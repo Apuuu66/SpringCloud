3b280bc4aaee56f09132702e8911e3a04bcc0480
Guiee <466007639@qq.com>
2020-04-29 17:09:23 +0800

    mq kafka

---
+++ mq/src/test/java/com/guier/mq/MqApplicationTests.java
@@ -0,0 +1,14 @@
+package com.guier.mq;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MqApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
