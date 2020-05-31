b680821bf75acd7f0ab21ed23d783003e1464fc5
Guiee <466007639@qq.com>
2020-05-31 12:57:29 +0800

    fix: config server sql 表名小写

--- config-server/src/main/resources/db/migration/V1.0.0__init_db.sql
+++ config-server/src/main/resources/db/migration/V1.0.0__init_db.sql
@@ -5,8 +5,8 @@ DROP TABLE IF EXISTS `config_center`;
 CREATE TABLE `config_center`
 (
     `id`          int(11)       NOT NULL AUTO_INCREMENT,
-    `key1`        varchar(255)  NULL DEFAULT NULL,
-    `value1`      varchar(255)  NULL DEFAULT NULL,
+    `key`        varchar(255)  NULL DEFAULT NULL,
+    `value`      varchar(255)  NULL DEFAULT NULL,
     `application` varchar(255)  NULL DEFAULT 'application',
     `profile`     varchar(255)  NULL DEFAULT 'dev',
     `label`       varchar(255)  NULL DEFAULT 'master',
